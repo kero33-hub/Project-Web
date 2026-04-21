6a631798d195cb23208ac717e347ecfa3089807a
kero33-hub <kerom620@gmail.com>
2026-04-21 15:50:53 +0200

    to Week 5

---
+++ script.js
@@ -0,0 +1,11 @@
+// Function 1: تغيير النص
+function changeText() {
+    document.getElementById("demo").innerHTML = "Text Changed 🔥";
+}
+
+
+
+// Function 3: رسالة للمستخدم
+function showMessage() {
+    alert("Welcome to Smart Parking 🚗");
+}
